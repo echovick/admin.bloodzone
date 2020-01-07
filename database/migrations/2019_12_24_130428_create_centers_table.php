0b475a6e21cb79f93044fc99d113d0e6858e5737
gabbyTI <gabrielibenye@gmail.com>
2020-01-07 23:18:46 +0100

    Updated the migration files for center and labScientists

--- database/migrations/2019_12_24_130428_create_centers_table.php
+++ database/migrations/2019_12_24_130428_create_centers_table.php
@@ -19,15 +19,13 @@ class CreateCentersTable extends Migration
             $table->string('name');
             $table->string('address');
             $table->string('phone_1');
-            $table->string('phone_2');
+            $table->string('phone_2')->nullable();
             $table->string('email')->unique();
             $table->string('state');
             $table->string('city');
-            $table->string('landmark');
+            $table->string('landmark')->nullable();
             $table->string('license');
             $table->string('status')->default('Pending Verification');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
 
         });
